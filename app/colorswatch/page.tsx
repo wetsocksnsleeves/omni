373ee8c36b549d22c7f4bc1b002d2cfc9a6caa32
wetsocksnsleeves <ectos69@gmail.com>
2025-09-30 20:18:08 +1000

    feat: Implemented skeleton logic to edit color swatches

--- app/colorswatch/page.tsx
+++ app/colorswatch/page.tsx
@@ -1,3 +1,7 @@
+"use client";
+import Image from "next/image";
+import { useState } from "react";
+
 interface Color {
     hexCode: string;
     label: string;
@@ -9,6 +13,8 @@ interface Swatch {
 }
 
 export default function ColorSwatch() {
+    const [edit, setEdit] = useState<number | null>(null);
+
     // Test swatch
     const exampleSwatch: Swatch = {
         label: "Test Swatch",
@@ -30,16 +36,23 @@ export default function ColorSwatch() {
 
     return (
         <div className="m-5 flex flex-col">
-            <div>
-                <h1 className="font-lora text-3xl">My swatches</h1>
-            </div>
             <div className="my-3 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 border border-border rounded-lg bg-primary w-full h-fit">
                 {data.map((swatch, index) => {
                     return (
                         <div
                             key={index}
-                            className="m-3 flex flex-col items-center justify-evenly h-fit border border-border bg-secondary rounded-md"
+                            className="m-3 p-3 flex flex-col items-center justify-evenly h-fit border border-border bg-secondary rounded-md min-h-60"
                         >
+                            {index == edit ? 
+                            <div className="w-full flex flex-col items-center justify-evenly">
+                            <text>Edit</text>
+                            <div className="w-full flex justify-between">
+                            <span className="">Remove</span>
+                            <span className="">Done</span>
+                            </div>
+                            </div>
+                            : 
+                            <div className="w-full">
                             <div
                                 className="mt-3 w-full grid grid-cols-2 gap-2 justify-items-center items-start lg:flex lg:flex-row justify-evenly
                             overflow-auto"
@@ -51,7 +64,7 @@ export default function ColorSwatch() {
                                             className="flex flex-col items-center max-w-25"
                                         >
                                             <div
-                                                className={`aspect-square w-25 h-25`}
+                                                className={`aspect-square w-25 h-25 rounded-md`}
                                                 style={{
                                                     backgroundColor:
                                                         color.hexCode,
@@ -64,7 +77,23 @@ export default function ColorSwatch() {
                                     );
                                 })}
                             </div>
-                            <label className="mb-3 lg:mt-0 mt-3">{swatch.label}</label>
+                            <div className="w-full flex justify-center items-center">
+                                <label className="ml-auto">
+                                    {swatch.label}
+                                </label>
+                                <button
+                                    className="ml-auto"
+                                    onClick={() => setEdit(index)}
+                                >
+                                    <Image
+                                        src="/edit.svg"
+                                        alt="Edit"
+                                        width="24"
+                                        height="24"
+                                    />
+                                </button>
+                            </div>
+                            </div>}
                         </div>
                     );
                 })}
